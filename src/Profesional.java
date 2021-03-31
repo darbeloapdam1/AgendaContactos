b52c9bca3126d58dd41a338fd2023e09b76b4d4c
cbarberiafdam1 <cbarberiafdam1@gmail.com>
2021-03-31 22:56:29 +0200

    Terminada la clase Profesional

--- src/Profesional.java
+++ src/Profesional.java
@@ -2,9 +2,17 @@
 public class Profesional extends Contacto {
 	
 	public String empresa;
-	
+
 	public Profesional(String nombre, String apellidos, String telefono, String email, String empresa) {
 		super(nombre, apellidos, telefono, email);
+		this.empresa = empresa;
+	}
+	
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
 		this.empresa = empresa;
 	}
 	
