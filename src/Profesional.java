8477469275b65c335bdd38208ccc8e5be9e20f59
cbarberiafdam1 <cbarberiafdam1@gmail.com>
2021-03-31 21:23:01 +0200

    Creado el constructor heredado

--- src/Profesional.java
+++ src/Profesional.java
@@ -1,8 +1,19 @@
 
-public class Profesional {
+public class Profesional extends Contacto {
+	
+	public String empresa;
+	
+	public Profesional(String nombre, String apellidos, String telefono, String email, String empresa) {
+		super(nombre, apellidos, telefono, email);
+		this.empresa = empresa;
+	}
+	
 
-	public Profesional() {
 
+	@Override
+	public String mensaje() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
