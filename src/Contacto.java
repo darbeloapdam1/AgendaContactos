c0197e6f27661e482b6c9de518947d55210d4e8e
cbarberiafdam1 <cbarberiafdam1@gmail.com>
2021-03-31 21:21:29 +0200

    Creada la clase Contacto

--- src/Contacto.java
+++ src/Contacto.java
@@ -1,4 +1,5 @@
-public class Contacto {
+
+public abstract class Contacto implements Comparable<Contacto> {
 	private String nombre;
 	private String apellidos;
 	private String telefono;
@@ -43,11 +44,55 @@ public class Contacto {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public char getPrimeraLetra() {
+		return apellidos.charAt(0);
+	}
+	
+	public abstract String mensaje();
+	
+	@Override
+	public String toString() {
+		String str = (apellidos + "," + nombre +"\nTfno: " + telefono + " | " + "email: " + email);
+		return str;
+	}
 
 	@Override
 	public int hashCode() {
 		return email.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (obj == this) {
+			return true;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		Contacto c = (Contacto) obj;
+		return c.getNombre().equals(this.nombre) && c.getApellidos().equals(this.apellidos) && 
+				c.getTelefono().equals(this.telefono) && c.getEmail().equals(this.email);
 
 	}
-
+	
+	@Override
+	public int compareTo(Contacto c1) {
+		if (this.getApellidos().compareToIgnoreCase(c1.getApellidos()) == 0) {
+			if (this.getNombre().compareToIgnoreCase(c1.getNombre()) == 0) {
+				return 0;
+			} else if (this.getNombre().compareToIgnoreCase(c1.getNombre()) > 0) {
+				return 1;
+			} else {
+				return -1;
+			}
+		}
+		if(this.getApellidos().compareToIgnoreCase(c1.getApellidos()) > 0) {
+			
+		}
+		return -1;
+	}
 }
