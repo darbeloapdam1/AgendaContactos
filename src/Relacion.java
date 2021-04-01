6ede617b0773d365dbdf32484b81fda13c549558
darbeloapdam1 <darbeloapdam1@gmail.com>
2021-04-01 12:37:03 +0200

    añadir constructor, parametros y metodo getRelacion()

--- src/Relacion.java
+++ src/Relacion.java
@@ -1,4 +1,15 @@
 
 public enum Relacion {
-	PADRE, MADRE, AMIGOS, PAREJA, HIJO, HIJA;
+	PADRE("padre"), MADRE("madre"), AMIGOS("amigos"), PAREJA("pareja"), HIJO("hijo"), HIJA("hija");
+	
+	private String relacion;
+
+	Relacion(String string) {
+		this.relacion = string;
+	}
+	
+	public String getRelacion() {
+		return relacion;
+	}
+	
 }
